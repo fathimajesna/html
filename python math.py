024f1f4eefab77b9f06a5cc8d4a8b793777dfa16
fathimajesna <148687969+fathimajesna@users.noreply.github.com>
2023-10-25 12:16:56 +0530

    Add files via upload

---
+++ python math.py
@@ -0,0 +1,16 @@
+#.........python math function.................
+import math
+#print the ceil value of 3.700
+print("ceil value:",math.ceil(3.700))
+#print the floor value of -5.12
+print("floor value:",math.floor(-5.12))
+#print the fab value of 9
+print("fab value",math.fabs(9))
+#print the factorial of 130
+print("factorial of",math.factorial(8))
+#print the gcd value of 4,12
+print("gcd value:",math.gcd(4,12))
+#print the square root of 4
+print("square root of 6:",math.sqrt(6))
+#print the value of pi
+print("pi value:",math.pi)
